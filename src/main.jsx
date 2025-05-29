fdcc9b83b70ae1d4991aa7b6487ef8b2d9a1df9a
Angie <angel890817@hotmail.com>
2025-05-29 18:16:27 +1000

    feature: set up exoense page

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserJwtProvider } from './context/UserJwtProvider.jsx'
 
 import './index.css'
 import App from './App.jsx'
 import Login from "./Login.jsx";
+import Expense from './pages/expense.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UserJwtProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/expenses" element={<Expense />} />
+        </Routes>
+      </BrowserRouter>
+    </UserJwtProvider>
   </StrictMode>,
-)
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
 );
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Login />
+//   </React.StrictMode>
+// );
