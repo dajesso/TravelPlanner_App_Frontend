e33137aaccd79c2f80c73420aa9052e89acb9663
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-06-07 09:26:33 +1000

    deleting object from vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html"
-      },
-    },
-  },
-});
+  plugins: [react()]
+  });
